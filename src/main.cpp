ef487b5d9e749f5ed988dbe758f18ecc1623b628
J. K. Lee <jklee95@users.noreply.github.com>
2022-01-01 00:50:08 +0900

    feat: Set orthogonal camera properties

--- src/main.cpp
+++ src/main.cpp
@@ -9,9 +9,9 @@ using namespace DXViewer::xmint3;
 
 int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE prevInstance, PSTR cmdLine, int showCmd)
 {
-    int isize = 30;
-    int jsize = 30;
-    int ksize = 30;
+    int isize = 60;
+    int jsize = 60;
+    int ksize = 60;
     double dx = 0.125;
     double timestep = 1.0 / 30.0;
 
@@ -20,10 +20,15 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE prevInstance, PSTR cmdLine, in
 
     // DirectX init
     DX12App* dxapp = new DX12App();
+    int maxSize = max_element(DirectX::XMINT3(isize, jsize, ksize));
+    //dxapp->setCameraProperties(
+    //    PROJ::PERSPECTIVE,
+    //    0.0f, maxSize * 0.25f,
+    //    0.0f, 0.0f);//-1.0f, -0.5f);
     dxapp->setCameraProperties(
-        PROJ::PERSPECTIVE,
-        0.0f, max_element(DirectX::XMINT3(isize, jsize, ksize)) * 0.25f,
-        0.0f, 0.0f);//-1.0f, -0.5f);
+        PROJ::ORTHOGRAPHIC,
+        maxSize * 0.0002f, maxSize * 0.2f,
+        0.0f, 0.0f);
     dxapp->setBackgroundColor(DirectX::Colors::LightSlateGray);
 
     // Window init
